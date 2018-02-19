4ceb069947403662a3153c787aeaa951606233b4
Stephen DuMont <watchformorning@gmail.com>
2018-02-19 05:10:48 -0500

    first

---
+++ Dialogs/RootDialog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace chatbot.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+            
+            var response = await InputHelper.ParseUserInput(activity.Text);
+            // return our reply to the user
+            await context.PostAsync(response.ToString());
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
---
+++ Helper/InputHelper.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace chatbot.Dialogs
+{
+    public static class InputHelper
+    {
+        public static async Task<UserInputInformation> ParseUserInput(string input)
+        {
+            UserInputInformation userInput = null;
+            
+            using (var client = new HttpClient())
+            {
+                string subscriptionKey = ConfigurationManager.AppSettings["LuisSubscriptionKey"].ToString();
+                string LuisAppId = ConfigurationManager.AppSettings["LuisAppId"].ToString();
+                string appGuid = ConfigurationManager.AppSettings["appGuid"].ToString();
+                string query = Uri.EscapeDataString(input);
+
+                string uri = $"https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/{appGuid}?subscription-key={subscriptionKey}&ID={LuisAppId}&q={query}";
+
+                try
+                {
+                    var response = await client.GetAsync(uri);
+                    response.EnsureSuccessStatusCode();
+
+                    var data = await response.Content.ReadAsStringAsync();
+                    userInput = JsonConvert.DeserializeObject<UserInputInformation>(data);
+                }
+                catch (Exception ex)
+                {
+
+                }
+                return userInput;
+            }
+        }
+    }
+}
